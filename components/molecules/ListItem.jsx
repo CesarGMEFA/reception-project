acaba69bae07d9b65a053501ef3adbec667808fc
Cesar Mejias <cesarfar376@gmail.com>
2022-09-23 20:49:24 -0400

    adding three css states to status data

--- components/molecules/ListItem.jsx
+++ components/molecules/ListItem.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from 'react'
 import Link from 'next/link'
 
 import { BsStopwatch } from 'react-icons/bs'
@@ -7,15 +8,30 @@ import { AiOutlineFilePdf } from 'react-icons/ai'
 import { getDeliveryTime } from '../../utils/getDeliveryTime'
 
 const ListItem = ({ data }) => {
-
+	let color;
 	const dateDelivery = getDeliveryTime(new Date(), data.delivery_time)
+
+	switch (data.status) {
+		case "reparación":
+			color = "text-blue-600";
+			break;
+		case "no reparado":
+			color = "text-red-600";
+			break;
+		case "entregado":
+			color = "text-green-600";
+			break;
+		
+		default:
+			break;
+	}
 
   return (
     <>
       <section className='grid grid-cols-5 gap-1 tablet:gap-3 w-full tablet:max-w-[700px]  bg-white py-4 px-4 my-1 border-b-2 rounded'>
 				<div className='col-[1/4] row-span-1 mobile:col-span-2'>
 					<span>#: {data.id}</span>
-					<span className='ml-4 w-28 text-blue-600 font-medium'>
+					<span className={`ml-4 w-28 text-blue-600 font-medium ${color}`}>
 						{data.status}
 					</span>
 				</div>
@@ -47,46 +63,12 @@ const ListItem = ({ data }) => {
 }
 
 export default ListItem
+{/* 
+<span className='ml-4 w-28 text-green-600 font-medium'>
+	delivered
+</span>
 
-
-{/* <section className='grid grid-cols-5 gap-1 tablet:gap-3 w-full tablet:max-w-[700px] bg-white py-4 px-4 my-1 border-b-2 rounded'>
-	<div className='col-[1/4] row-span-1 mobile:col-span-2'>
-		<span>#: 1706</span>
-		<span className='ml-4 w-28 text-green-600 font-medium'>
-			delivered
-		</span>
-	</div>
-	<div className='col-[1/5] row-[2/3] mobile:row-span-1 mobile:col-[3/5]'>
-		<span className='mr-4'>Johanna T</span>
-		<span>iphone X</span>
-	</div>
-	<div className='col-[4/6] mobile:col-[5] mobile:row-span-1 justify-self-end'>
-		<BsStopwatch className='inline-block' />
-		<span className='ml-2'>30 minutos</span>
-	</div>
-	<div className='col-[5/6]  mobile:col-[6] mobile:row-span-1 justify-self-end'>
-		<ImEye className='inline-block mr-2' />
-		<AiOutlineFilePdf className='inline-block' />
-	</div>
-</section>
-
-<section className='grid grid-cols-5 gap-1 tablet:gap-3 w-full tablet:max-w-[700px] bg-white py-4 px-4 my-1 border-b-2 rounded'>
-	<div className='col-[1/4] row-span-1 mobile:col-span-2'>
-		<span>#: 34</span>
-		<span className='ml-4 w-28 text-red-600 font-medium'>
-			Undelivered
-		</span>
-	</div>
-	<div className='col-[1/5] row-[2/3] mobile:row-span-1 mobile:col-[3/5]'>
-		<span className='mr-4'>Theodore R</span>
-		<span>iphone 13 pro</span>
-	</div>
-	<div className='col-[4/6] mobile:col-[5] mobile:row-span-1 justify-self-end'>
-		<BsStopwatch className='inline-block' />
-		<span className='ml-2'>5 horas</span>
-	</div>
-	<div className='col-[5/6]  mobile:col-[6] mobile:row-span-1 justify-self-end'>
-		<ImEye className='inline-block mr-2' />
-		<AiOutlineFilePdf className='inline-block' />
-	</div>
-</section> */}
+<span className='ml-4 w-28 text-red-600 font-medium'>
+	Undelivered
+</span>
+*/}
